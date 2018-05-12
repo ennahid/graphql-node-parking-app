4e2585cfc48ea91ebae496a3db687da431522091
ADIL2HAMMAD <jacezaz@gmail.com>
2018-05-12 17:55:52 +0100

    ok

--- index.js
+++ index.js
@@ -8,44 +8,51 @@ mongoose.connect('mongodb://localhost/parkingdb');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('connected')
+    console.log('connected')
 });
 
 var UsersSchema = mongoose.Schema({
-  name: String
+    name: String
 });
 
 UsersSchema.methods.showUser = () => {
-  var greeting = this.name
-    ? "my name is " + this.name
-    : "I don't have a name";
-  console.log(greeting);
+    var greeting = this.name ?
+        "my name is " + this.name :
+        "I don't have a name";
+    console.log(greeting);
 }
 
 var User = mongoose.model('user', UsersSchema);
 
-var user1 = new User({ name: 'ennahid' });
+var user1 = new User({ name: 'SIMO' });
 // user1.showUser();
 
 user1.save((err) => {
-  if (err) return console.error(err);
-  console.log('done');
+    if (err) return console.error(err);
+    console.log('done');
 });
 
-var Users;
+/* var Users;
 
 
 User.find((err, users) => {
-  if (err) return console.error(err);
-  Users = users;
+    if (err) return console.error(err);
+    Users = users;
+}); */
+var Users;
+app.get('/', function(req, res) {
+    res.send("home");
 });
 
-app.get('/', function(req, res){
-   res.send("home");
-});
-app.get('/users', function(req, res){
 
-  res.send(Users);
-});
 
+app.get('/users', function(req, res) {
+
+    User.find((err, users) => {
+        if (err) return console.error(err);
+        Users = users;
+    });
+    res.send(Users);
+
+});
 app.listen(3000);
