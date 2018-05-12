dc149712e6b12031bce77425399387f1be76c70a
ennahid <abdelghafo@gmail.com>
2018-05-12 19:00:44 +0100

    user search api

--- index.js
+++ index.js
@@ -31,28 +31,39 @@ var User = mongoose.model('user', UsersSchema);
 var user1 = new User({ name: 'ennahid' });
 // user1.showUser();
 
-user1.save((err) => {
-  if (err) return console.error(err);
-  console.log('done');
-});
 
-var Users;
-
-User.find((err, users) => {
-  if (err) return console.error(err);
-  Users = users;
-});
 
 app.get('/', function(req, res){
    res.send("home");
 });
+
 app.get('/users', function(req, res){
-  res.send(Users);
+
+  User.find((err, users) => {
+    if (err) return console.error(err);
+    res.send(users);
+  });
+
 });
 
 
-app.post('/insert', function (req, res) {
-  //returns the 
-  res.send(JSON.stringify(req.body));
+app.get('/user/:name', (req, res) => {
+  User.find({ 'name': req.params.name }, 'name',(err, user) => {
+    if (err) return handleError(err);
+    // Prints "Space Ghost is a talk show host".
+    res.send(JSON.stringify(user));
+  });
+
+});
+
+
+
+app.post('/insertUser', function (req, res) {
+  //returns the form data
+  var NewUser = new User({name : req.body.name});
+  NewUser.save((err) => {
+    if (err) res.send('error');
+    res.send(JSON.stringify(req.body));
+  });
 });
 
