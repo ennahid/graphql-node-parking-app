c3bc2a524e5270cb639c04331b8f79f481fba378
ADIL2HAMMAD <jacezaz@gmail.com>
2018-05-15 11:59:46 +0000

    Correct changes Working

--- index.js
+++ index.js
@@ -14,60 +14,36 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('connected')
-});
-
-var UsersSchema = mongoose.Schema({
-    name: String
-});
-
-UsersSchema.methods.showUser = () => {
-    var greeting = this.name ?
-        "my name is " + this.name :
-        "I don't have a name";
-    console.log(greeting);
-}
-
-/* var User = mongoose.model('user', UsersSchema);
-
-var user1 = new User({ name: 'SIMO' });
-// user1.showUser();
-
-user1.save((err) => {
-    if (err) return console.error(err);
-    console.log('done');
-}); */
-
-/* var Users;
-
-app.get('/user/:name', (req, res) => {
-  // User.find({ 'name': req.params.name }, 'name',(err, user) => {
-  //   if (err) return handleError(err);
-  //   // Prints "Space Ghost is a talk show host".
-  //   res.send(JSON.stringify(user));
-  // });
-
-User.find((err, users) => {
-    if (err) return console.error(err);
-    Users = users;
-}); */
-var Users;
 app.get('/', function(req, res) {
     res.send("home");
 });
-
-
 
 app.get('/users', function(req, res) {
 
     User.find((err, users) => {
         if (err) return console.error(err);
-        Users = users;
+        res.send(users);
     });
-    res.send(Users);
 
 });
-app.listen(3000);
+
+
+app.get('/user/:name', (req, res) => {
+    // User.find({ 'name': req.params.name }, 'name',(err, user) => {
+    //   if (err) return handleError(err);
+    //   // Prints "Space Ghost is a talk show host".
+    //   res.send(JSON.stringify(user));
+    // });
+
+});
+
+
+
+app.post('/insertUser', function(req, res) {
+    //returns the form data
+    var NewUser = new User({ name: req.body.name });
+    NewUser.save((err) => {
+        if (err) res.send('error');
+        res.send(JSON.stringify(req.body));
+    });
+});
